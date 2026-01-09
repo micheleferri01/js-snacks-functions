a1dae7be83bf7d9623a93a4448081570c950eb87
micheleferri01 <micheleferri095@gmail.com>
2026-01-09 16:20:27 +0100

    Dichiarazione funzione (05_bonus)

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,7 +9,30 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+/**
+ * genera un messaggio di saluto diverso in base all'ora attuale
+ * @param {string} name nome di chi salutare
+ * @returns {string} saluto
+ */
+function greet(name){
+    // variabile dove salvare l'orario
+    const hour = new Date().getHours();
+    // "Jenuary 09, 2026 18:15:00" copiare all'interno delle parentesi di new Date per testare se funziona con orari diversi
 
+    // viaribile dove salvare il messaggio
+    let message = "";
+    if (hour < 13){
+        message = `Buongiorno ${name}`;
+    } 
+    else if (hour < 17) {
+        message = `Buon pomeriggio ${name}`;
+
+    }
+    else {
+        message = `Buonasera ${name}`;
+    }
+    return message
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
