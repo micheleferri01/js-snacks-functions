6e14bd2f05fbdd3405de3091b56d76915ee8e74e
micheleferri01 <micheleferri095@gmail.com>
2026-01-09 15:38:10 +0100

    Dichirazione funzione (04_conta_vocali)

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,6 +5,24 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+/**
+ * restituisce un array con tutte le vocali della parola data
+ * @param {string} word parola da controllare
+ * @returns {object} array con tutte le vocali nella parola
+ */
+function vocalsInString(word){
+    const vocals = ['a', 'e', 'i', 'o', 'u'];
+    const vocalsinWord = [];
+    for(let i = 0; i < word.length; i++){
+        const currentChar = word[i];
+        for(let i = 0; i < vocals.length; i++){
+            if (currentChar === vocals[i]){
+                vocalsinWord.push(currentChar);
+            }
+        }
+    }
+    return vocalsinWord;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
