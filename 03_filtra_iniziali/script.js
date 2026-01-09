2622fe71ce07f81bfeb945c939998dcaac820344
micheleferri01 <micheleferri095@gmail.com>
2026-01-09 15:03:44 +0100

    Dichiarazione funzione (03_filtra_iniziali)

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,25 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+/**
+ * Ritorna un array con tutti gli elementi che iniziano con la lettera cercata
+ * @param {string} letter lettera da cercare
+ * @param {object} array array in cui cercare
+ * @returns {object} array con tutti gli elementi che inziano con la lettera cercata
+ */
+function startsWith(letter,array){
+    const namesStartingWith = [];
+    for(let i = 0; i < array.length; i++){
+        const currentElement = array[i];
+        if (currentElement.charAt(0) === letter){
+            namesStartingWith.push(array[i]);
+        }
 
+    }
+    return namesStartingWith;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
